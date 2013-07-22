5eb4bea03c1fc9e27f6cf72863ea95ff12e18e36
StanDroid888 <standroid888@gmail.com>
2013-07-22 15:42:06 -0400

    updated methods for Queue.java

--- Queue_Integers/Queue.java
+++ Queue_Integers/Queue.java
@@ -1,3 +1,9 @@
+/**
+ * @author Stanley Wong
+ * This class is a implementation of a Dynamic Queue.
+ * Node.java is required for this class.  
+ *
+ */
 public class Queue {
 
 	int queueSize;
@@ -21,30 +27,31 @@ public class Queue {
 		return head;
 	}
 
-	public void setHead(Node head) {
-		this.head = head;
-	}
-
 	public Node getTail() {
 		return tail;
 	}
 
-	public void setTail(Node tail) {
-		this.tail = tail;
-	}
-
-	// Return TRUE if queue is empty
+	/**
+	 * checks to see if a Queue is empty. 
+	 * @return
+	 */
 	public boolean isEmpty() {
-		return (head == null && tail == null ? true : false);
+		return (head == null && tail == null && this.queueSize==0 ? true : false);
 	}
 
+	/**
+	 * This method is used to add a new item to the queue with the 
+	 * data provided in the parameters. All additions will be done 
+	 * at the tail of the queue. 
+	 * @param data
+	 */
 	public void addToQueue(Integer data) {
 		// All new nodes will be added to the
 		// tail of the Queue.
 
 		Node newNode = new Node(data);
 		newNode.setNextNode(null);
-		queueSize++;
+		
 
 		if (isEmpty()) {
 			// This will be the only Node in the Queue.
@@ -67,8 +74,38 @@ public class Queue {
 			tail = newNode;
 
 		}
+		
+		// Increase the Queue Size by one.
+		queueSize++;
 	}
 
+	
+	/**
+	 * This method removes an item from the head of the Queue. 
+	 * A soft copy of the Node will be returned. 
+	 * @return
+	 */
+	public Node removeFromQueue() {
+		
+		Node deletedNode = new Node(); 
+		
+		// Set the data in the return node with
+		// what is currently in the head node;
+		deletedNode.setData(getHead().getData());
+		
+		// Now set the head node to head->next
+		// so that the garbage collector will 
+		// get rid of the node originally at the
+		// head of the queue;
+		head = getHead().getNextNode();	
+		
+		return(deletedNode);
+	}
+	
+	/**
+	 * This method displays the queue in a list format
+	 * to standard output. 
+	 */
 	public void showQueue() {
 
 		Node currentNode = getHead();
--- Queue_Integers/QueueDriver.java
+++ Queue_Integers/QueueDriver.java
@@ -8,6 +8,11 @@ public class QueueDriver {
 		TestQueue.addToQueue(1);
 		TestQueue.addToQueue(2);
 		TestQueue.addToQueue(3);
+		TestQueue.addToQueue(4);
+		TestQueue.addToQueue(5);
+		TestQueue.addToQueue(6);
+		
+		System.out.println("Node with value: " + TestQueue.removeFromQueue().getData() +" removed.");
 		
 		TestQueue.showQueue();
 	}
