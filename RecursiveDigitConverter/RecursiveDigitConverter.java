8812816714c38a73cfb05cb8c8fed52a9ecb95e5
StanDroid888 <standroid888@gmail.com>
2013-07-27 18:26:28 -0400

    update comments

--- RecursiveDigitConverter/RecursiveDigitConverter.java
+++ RecursiveDigitConverter/RecursiveDigitConverter.java
@@ -23,25 +23,64 @@ public class RecursiveDigitConverter {
 		this.convertedValue = 0;
 	}
 
+	/**
+	 * This method is used to calculate the numerical value of "convertedValue"
+	 * via recursion. Each time the method is called recursively, the parameter
+	 * which contains the index of a character in the digitsString is
+	 * incremented by one to get to the next character. As each character is
+	 * processed, the charIndex is used to determine what place value (base 10)
+	 * is to be used. The integer 10 is raised to the the exponent of (the
+	 * length of the digits string subtracted by the charIndex) and then that
+	 * calculation is added to the "convertedValue" at each recursive call. The
+	 * base case for this method is when the charIndex is equal to the length of
+	 * the digits string to be evaluated. Since there is no character at that
+	 * index, the method just returns.
+	 * 
+	 * @param charIndex (must start at zero)
+	 */
 	public void covertToInteger(int charIndex) {
 
 		int tempInt = 0;
 		Character tempChar;
 
+		/*
+		 * BASE CASE: 
+		 * charIndex has incremented to the point that it is equal to
+		 * the length of the digits string.
+		 */
 		if (charIndex == this.digitsString.length()) {
 			return;
 		} else {
+			/*
+			 * Continue recursively with a incremented
+			 * value for charIndex. This conditional 
+			 * will get the character at the charIndex then
+			 * convert that character into its integer value.
+			 */
 			tempChar = this.digitsString.charAt(charIndex);
 			tempInt = Character.getNumericValue(tempChar);
 
+			/*
+			 * Raise 10 to the power of the difference between the digits string's 
+			 * length and the charIndex. Afterwards, multiple by the integer value
+			 * which was received and add that product to the convertedValue attribute. 
+			 */
 			this.convertedValue = (int) (this.convertedValue + tempInt
 					* Math.pow(10, this.digitsString.length() - charIndex - 1));
 
+			/*
+			 * Recursive call to continue
+			 */
 			covertToInteger(charIndex + 1);
 
 		}
 	}
 
+	
+	/**
+	 * Getter to return converted value.
+	 * @return
+	 */
 	public int getConvertedValue() {
 		return convertedValue;
 	}
--- RecursiveDigitConverter/RecursiveDigitConverterDriver.java
+++ RecursiveDigitConverter/RecursiveDigitConverterDriver.java
@@ -2,7 +2,7 @@
  * 
  * Programmer: Stanley Wong
  * 
- * This is a driver for the RecursivePrinter class.
+ * This is a driver for the RecursiveDigitConverterDriver class.
  * 
  */
 public class RecursiveDigitConverterDriver {
@@ -18,7 +18,27 @@ public class RecursiveDigitConverterDriver {
 
 		RecursiveDigitConverter dc1 = new RecursiveDigitConverter("1234");
 		dc1.covertToInteger(0);
-		System.out.println("This is the covertered value:" + dc1.getConvertedValue());
+		System.out.println("This is the covertered value:"
+				+ dc1.getConvertedValue());
+
+		RecursiveDigitConverter dc2 = new RecursiveDigitConverter("1200256");
+		dc2.covertToInteger(0);
+		System.out.println("This is the covertered value:"
+				+ dc2.getConvertedValue());
+
+		RecursiveDigitConverter dc3 = new RecursiveDigitConverter("808080");
+		dc3.covertToInteger(0);
+		System.out.println("This is the covertered value:"
+				+ dc3.getConvertedValue());
+
+		/*
+		 * Overflow
+		 */
+		RecursiveDigitConverter dc4 = new RecursiveDigitConverter(
+				"327481704303198739021431098");
+		dc4.covertToInteger(0);
+		System.out.println("This is the covertered value:"
+				+ dc4.getConvertedValue());
 
 	}
 }
